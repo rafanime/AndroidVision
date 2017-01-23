6dd4609265e056419bc9ef48819c3155fd188a23
Rafael Nunes <rafael@keypla.com>
2017-01-23 14:15:11 +0000

    Resize and pause handled

--- app/src/main/java/com/androidvision/Camera/CameraPreview.java
+++ app/src/main/java/com/androidvision/Camera/CameraPreview.java
@@ -6,6 +6,7 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * Created by rafanime on 23/01/2017.
@@ -51,11 +52,56 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
         }
 
         try {
+            Camera.Parameters params = mCamera.getParameters();
+            List<Camera.Size> sizes = params.getSupportedPreviewSizes();
+            Camera.Size optimalSize = getOptimalPreviewSize(sizes,w,h);
+            params.setPreviewSize(optimalSize.width,optimalSize.height);
+
+            params.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+
+
+            mCamera.setParameters(params);
+
             mCamera.setPreviewDisplay(mHolder);
             mCamera.startPreview();
-
+            mCamera.autoFocus(null);
+            
         } catch (Exception e){
 
         }
+    }
+
+
+    private Camera.Size getOptimalPreviewSize(List<Camera.Size> sizes, int w, int h) {
+
+        final double ASPECT_TOLERANCE = 0.1;
+        double targetRatio=(double)h / w;
+
+        if (sizes == null) return null;
+
+        Camera.Size optimalSize = null;
+        double minDiff = Double.MAX_VALUE;
+
+        int targetHeight = h;
+
+        for (Camera.Size size : sizes) {
+            double ratio = (double) size.width / size.height;
+            if (Math.abs(ratio - targetRatio) > ASPECT_TOLERANCE) continue;
+            if (Math.abs(size.height - targetHeight) < minDiff) {
+                optimalSize = size;
+                minDiff = Math.abs(size.height - targetHeight);
+            }
+        }
+
+        if (optimalSize == null) {
+            minDiff = Double.MAX_VALUE;
+            for (Camera.Size size : sizes) {
+                if (Math.abs(size.height - targetHeight) < minDiff) {
+                    optimalSize = size;
+                    minDiff = Math.abs(size.height - targetHeight);
+                }
+            }
+        }
+        return optimalSize;
     }
 }
--- app/src/main/java/com/androidvision/MainActivity.java
+++ app/src/main/java/com/androidvision/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.androidvision.Camera.CameraPreview;
@@ -109,4 +110,16 @@ public class MainActivity extends AppCompatActivity {
         stopCamera();
     }
 
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            findViewById(R.id.full_view).setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
 }
