9f92dd7d9d12102e266ca653d6169934708c2b1f
Rafael Nunes <rafael@keypla.com>
2017-01-27 17:03:29 +0000

    permissions fixed

--- app/src/main/java/com/androidvision/MainActivity.java
+++ app/src/main/java/com/androidvision/MainActivity.java
@@ -26,7 +26,8 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
 
-        checkPermissions();
+        if(checkPermission(Manifest.permission.CAMERA, MY_PERMISSIONS_REQUEST_CAMERA))
+            startCamera();
 
         Button switchButton = (Button) findViewById(R.id.Switch);
         switchButton.setOnClickListener(
@@ -34,8 +35,10 @@ public class MainActivity extends Activity {
                     @Override
                     public void onClick(View v) {
                         switchCamera();
-                        stopCamera();
-                        checkPermissions();
+                        if(checkPermission(Manifest.permission.CAMERA, MY_PERMISSIONS_REQUEST_CAMERA)) {
+                            stopCamera();
+                            startCamera();
+                        }
                     }
                 }
         );
@@ -44,39 +47,17 @@ public class MainActivity extends Activity {
 
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
 
-    private void checkPermissions() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.READ_CONTACTS)
-                != PackageManager.PERMISSION_GRANTED) {
+    private boolean checkPermission(String permission, int request){
+        int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
+                permission);
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.CAMERA)) {
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{permission},
+                    request);
+            return false;
+        } else return true;
 
-            } else {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.CAMERA},
-                        MY_PERMISSIONS_REQUEST_CAMERA);
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CAMERA: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Log.d("Success -> " , " We have camera permission");
-                    startCamera();
-
-                } else {
-                }
-                return;
-            }
-
-        }
     }
 
     private void startCamera () {
