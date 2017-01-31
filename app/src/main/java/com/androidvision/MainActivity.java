db2a374eea5929c74f550c57c806fa194cc85af2
Rafael Nunes <rafael.lourenco.nunes@gmail.com>
2017-01-31 17:53:29 +0000

    changed camera to openCV, filters added

--- app/src/main/java/com/androidvision/MainActivity.java
+++ app/src/main/java/com/androidvision/MainActivity.java
@@ -6,18 +6,29 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.view.SurfaceView;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import static org.opencv.imgproc.Imgproc.ADAPTIVE_THRESH_MEAN_C;
+import static org.opencv.imgproc.Imgproc.THRESH_BINARY;
 
 public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private CameraBridgeViewBase mOpenCvCameraView;
+    private PortraitCameraView mOpenCvCameraView;
 
     static {
         if (!OpenCVLoader.initDebug()) {
@@ -35,13 +46,51 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
-        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.camera_preview);
+        mOpenCvCameraView = (PortraitCameraView) findViewById(R.id.camera_preview);
+
+
+        mOpenCvCameraView.setVisibility(View.VISIBLE);
         mOpenCvCameraView.setCvCameraViewListener(this);
+
         mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
 
+
+        // Add a listener to the Capture button
+        Button captureButton = (Button) findViewById(R.id.button_capture);
+        captureButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+
+                        //if(checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE, MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE))
+                          //  myCamera.takePicture(null, null, mPicture);
+                    }
+                }
+        );
+
+        Button filterButton = (Button) findViewById(R.id.Threshold);
+        filterButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adaptiveThreshold = !adaptiveThreshold;
+            }
+        });
+
+        Button greyButton = (Button) findViewById(R.id.Grey);
+        greyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                greyFilter = !greyFilter;
+            }
+        });
     }
 
+
+
+
     private static final int MY_PERMISSIONS_REQUEST_CAMERA = 0;
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
 
     private boolean checkPermission(String permission, int request){
         int permissionCheck = ContextCompat.checkSelfPermission(getApplicationContext(),
@@ -55,6 +104,7 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         } else return true;
 
     }
+
 
     BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
         @Override
@@ -102,7 +152,26 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
     }
 
 
+    private boolean adaptiveThreshold = false;
+    private boolean greyFilter = false;
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
-        return inputFrame.rgba();
+
+        if(adaptiveThreshold) {
+            Mat mRgba = inputFrame.gray();
+            Mat mRgbaT = mRgba.t();
+
+            Imgproc.adaptiveThreshold(mRgba, mRgbaT, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 15, 40);
+
+            return mRgbaT;
+        } else if(greyFilter) {
+            return inputFrame.gray();
+        }
+        else return inputFrame.rgba();
+
+
+
+
     }
+
+
 }
